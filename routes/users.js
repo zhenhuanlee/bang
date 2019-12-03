bb40a339fbc9950b7132bd1b7433718da07a4492
zhenhuanlee <zhenhuanlee@outlook.com>
2019-12-03 17:17:09 +0800

    redis

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
+var redisClient = require('../lib/redis')
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  redisClient.set("hi", "nihao");
+  redisClient.get("hi", function(err, reply) {
+    console.log(reply);
+    res.send(reply.toString());
+  });
 });
 
 module.exports = router;
